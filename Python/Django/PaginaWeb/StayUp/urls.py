e9005ad6ae357068c5a8115bbb621fa872812a7d
Gael Arzubide <arzubide.ga@gmail.com>
2025-04-22 15:11:16 -0600

    correxion

--- Python/Django/PaginaWeb/StayUp/urls.py
+++ Python/Django/PaginaWeb/StayUp/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('applications.ResgistroSesion.urls')),
-    path('', include('applications.AdminControl.urls'))
+    path('', include('applications.AdminControl.urls')),
 ]
